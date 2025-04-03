7c7fa793e47a843509d34712414389ffc3e0dfe9
marvindmartian2828 <166263571+marvindmartian2828@users.noreply.github.com>
2025-04-03 12:54:24 -0600

    Update test in App.test.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Leona Ilao</h1> 
+      <h1>Leona Ilao</h1>
+      <p>Enterprise Computing: TECH2102-25JANMNFS2</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders your name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Leona Ilao/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Leona Ilao/i); 
+  expect(nameElement).toBeInTheDocument();
 });
